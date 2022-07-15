e3085ff4ab4811ead6c3c6beb6dc3fdac62201fe
Victor Prado <prado.victor1005@gmail.com>
2022-07-14 23:19:14 -0300

    criando os componentes de input, select e button

--- fe/src/pages/NewContact/index.js
+++ fe/src/pages/NewContact/index.js
@@ -1,9 +1,29 @@
 import PageHeader from '../../components/PageHeader';
 
+import Input from '../../components/input';
+import Select from '../../components/select';
+import Button from '../../components/button';
+
 export default function Home() {
   return (
-    <PageHeader
-      title="Novo contato"
-    />
+    <>
+      <PageHeader
+        title="Novo contato"
+      />
+      <Input type="text" placeholder="Nome" />
+      <Select>
+        <option value="123">Instagram</option>
+        <option value="123">Instagram</option>
+        <option value="123">Instagram</option>
+        <option value="123">Instagram</option>
+        <option value="123">Instagram</option>
+      </Select>
+      <Button type="button">
+        Salvar alterações
+      </Button>
+      <Button type="button" disabled>
+        Salvar alterações
+      </Button>
+    </>
   );
 }
